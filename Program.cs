b9f6a4d3aae78792598b9336590accbc786e406e
AndreyKerges <andrey.nascimento4@etec.sp.gov.br>
2023-03-03 22:30:10 -0300

    Finalizando exercicio do pastorzinho

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Faça um programa que emita beeps no tom da canção citada em O Pastorzinho");
+Console.WriteLine();
+Console.WriteLine("Dó, ré, mi, fá, fá, fá,");
+Console.WriteLine("Dó, ré, dó, ré, ré, ré,");
+Console.WriteLine("Dó, sol, fá, mi, mi, mi,");
+Console.WriteLine("Dó, ré, mi, fá, fá, fá.");
